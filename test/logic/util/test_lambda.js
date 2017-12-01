e33106eaba1652554089993821d86246ef18c4fd
blackflux <blackflux@unfine.de>
2017-11-30 21:31:45 -0800

    - improved coverage

--- test/logic/util/test_lambda.js
+++ test/logic/util/test_lambda.js
@@ -24,6 +24,28 @@ describe("Testing Lambda", () => {
     });
   });
 
+  it("Testing appendTags Error", (done) => {
+    nockBack.fixtures = path.join(__dirname, "__cassette");
+    nockBack(`lambda-get-append-tags-error.json_recording.json`, {}, (nockDone) => {
+      lambdaInvalid.appendTags([{FunctionArn: "FUNCTION_ARN"}]).catch((e) => {
+        expect(e.message).to.equal("The security token included in the request is invalid.");
+        nockDone();
+        done();
+      });
+    });
+  });
+
+  it("Testing setCloudWatchRetention Error", (done) => {
+    nockBack.fixtures = path.join(__dirname, "__cassette");
+    nockBack(`lambda-get-set-cloud-watch-retention-error.json_recording.json`, {}, (nockDone) => {
+      lambdaInvalid.setCloudWatchRetention({FunctionName: "FUNCTION_NAME"}).catch((e) => {
+        expect(e.message).to.equal("The security token included in the request is invalid.");
+        nockDone();
+        done();
+      });
+    });
+  });
+
   it("Testing getAllFunctions Batched", (done) => {
     nockBack.fixtures = path.join(__dirname, "__cassette");
     nockBack(`lambda-get-all-functions.json_recording.json`, {}, (nockDone) => {
