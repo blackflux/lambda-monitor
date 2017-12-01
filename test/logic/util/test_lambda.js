1cd5f140e89cc704c302073cf58af2c4418e7970
blackflux <blackflux@unfine.de>
2017-11-30 21:32:24 -0800

    - fixed eslint

--- test/logic/util/test_lambda.js
+++ test/logic/util/test_lambda.js
@@ -27,7 +27,7 @@ describe("Testing Lambda", () => {
   it("Testing appendTags Error", (done) => {
     nockBack.fixtures = path.join(__dirname, "__cassette");
     nockBack(`lambda-get-append-tags-error.json_recording.json`, {}, (nockDone) => {
-      lambdaInvalid.appendTags([{FunctionArn: "FUNCTION_ARN"}]).catch((e) => {
+      lambdaInvalid.appendTags([{ FunctionArn: "FUNCTION_ARN" }]).catch((e) => {
         expect(e.message).to.equal("The security token included in the request is invalid.");
         nockDone();
         done();
@@ -38,7 +38,7 @@ describe("Testing Lambda", () => {
   it("Testing setCloudWatchRetention Error", (done) => {
     nockBack.fixtures = path.join(__dirname, "__cassette");
     nockBack(`lambda-get-set-cloud-watch-retention-error.json_recording.json`, {}, (nockDone) => {
-      lambdaInvalid.setCloudWatchRetention({FunctionName: "FUNCTION_NAME"}).catch((e) => {
+      lambdaInvalid.setCloudWatchRetention({ FunctionName: "FUNCTION_NAME" }).catch((e) => {
         expect(e.message).to.equal("The security token included in the request is invalid.");
         nockDone();
         done();
