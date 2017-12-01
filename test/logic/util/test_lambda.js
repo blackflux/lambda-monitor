d9c2b74170d5fc0f9c99bb43ccb7bf2d53e13a3a
blackflux <blackflux@unfine.de>
2017-11-30 21:25:52 -0800

    - fixed eslint issues

--- test/logic/util/test_lambda.js
+++ test/logic/util/test_lambda.js
@@ -1,19 +1,22 @@
 const path = require("path");
 const expect = require("chai").expect;
 const nockBack = require('nock').back;
+const lambda = require("../../../lib/logic/util/lambda")({
+  region: "us-east-1"
+});
+const lambdaInvalid = require("../../../lib/logic/util/lambda")({
+  region: "us-east-1",
+  accessKeyId: "INVALID_ACCESS_KEY_ID",
+  secretAccessKey: "INVALID_SECRET_ACCESS_KEY"
+});
 
 nockBack.setMode('record');
 
 describe("Testing Lambda", () => {
   it("Testing getAllFunctions Error", (done) => {
-    const lambda = require("../../../lib/logic/util/lambda")({
-      region: "us-east-1",
-      accessKeyId: "INVALID_ACCESS_KEY_ID",
-      secretAccessKey: "INVALID_SECRET_ACCESS_KEY"
-    });
     nockBack.fixtures = path.join(__dirname, "__cassette");
     nockBack(`lambda-get-all-functions-error.json_recording.json`, {}, (nockDone) => {
-      lambda.getAllFunctions().catch(e => {
+      lambda.getAllFunctions().catch((e) => {
         expect(e.message).to.equal("The security token included in the request is invalid.");
         nockDone();
         done();
@@ -22,12 +25,9 @@ describe("Testing Lambda", () => {
   });
 
   it("Testing getAllFunctions Batched", (done) => {
-    const lambda = require("../../../lib/logic/util/lambda")({
-      region: "us-east-1"
-    });
     nockBack.fixtures = path.join(__dirname, "__cassette");
     nockBack(`lambda-get-all-functions.json_recording.json`, {}, (nockDone) => {
-      lambda.getAllFunctions().then(r => {
+      lambdaInvalid.getAllFunctions().then((r) => {
         expect(r.length).to.equal(2);
         nockDone();
         done();
