73e5a7114ae5c5322b3dddeba92b07056ebe7161
blackflux <blackflux@unfine.de>
2017-12-05 20:43:01 -0800

    chore: added test to ensure log levels are correctly detected for rollbar

---
+++ test/logic/test_processLogs.js
@@ -0,0 +1,23 @@
+const expect = require("chai").expect;
+const processLogs = require("../../lib/logic/processLogs");
+
+describe("Testing processLogs", () => {
+  it("Testing Log Level Extraction", (done) => {
+    const logs = [];
+    processLogs({
+      awslogs: {
+        data: (
+          "H4sIAHp0J1oA/zWQzW7bMBCEX0UgerQiLv+W0k1AnaBAmx6sUxOjWFErQ6gtGqJsowjy7mGa5sidGX6YeREnTokO3P09s2jE17Zrf//Y7n" +
+          "btw1ZsRLzNvOQzeu1UDeBMjfl8jIeHJV7OWanolqojnfqBqjMfyhSP0zrRwuXA1zJeeblOfPvI7NaF6ZRDSgJWAJXS1dOX72233XV7dl6O" +
+          "rFVwPRkf2AdNGpGUDYPxnvMX6dKnsEzndYrz/XRceUmieRKPceaPp9j/42yvPK/v0ouYhozTGqU1iAZ0jTLDrXZWSovWOYe1QilR6loZbR" +
+          "CsAUDna21cRq5T3melU64KFsBI67F2zm4+d/vfpgQole6kaiw2tr7Lll8FBEU4ai7DOFJ2MJZE2JeDDoOVI8rQy+Lb4/3Ppugo/SneYUMR" +
+          "L2tBY25TwJ2UReIQ5yE9z8+zeN2/vgGRLcfOsQEAAA=="
+        )
+      }
+    }, null, (err) => {
+      expect(logs).to.deep.equal(["INFO: Task timed out after 1.00 seconds\n\n"]);
+      expect(err).to.equal(null);
+      done();
+    }, { info: msg => logs.push(msg.message) });
+  });
+});
