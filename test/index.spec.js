8bf4293110f2570d304e41f925db66e4a74c7de8
Lukas Siemon <lukas.siemon@gmail.com>
2019-04-14 16:43:19 -0700

    fix: updated js-gardener

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,7 +2,7 @@ const path = require('path');
 const lambdaTester = require('lambda-tdd')({
   cwd: path.join(__dirname, '..'),
   verbose: process.argv.slice(2).indexOf('--verbose') !== -1,
-  handlerFile: path.join(__dirname, '..', 'src', 'handler.js'),
+  handlerFile: path.join(__dirname, '..', 'src', 'index.js'),
   cassetteFolder: path.join(__dirname, 'lambda', '__cassettes'),
   envVarYml: path.join(__dirname, 'lambda', 'env.yml'),
   envVarYmlRecording: path.join(__dirname, 'lambda', 'env.recording.yml'),
