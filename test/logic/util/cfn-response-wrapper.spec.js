534b27b29f2676775b4975eae42c5e4db32f77b3
Kodi Ackerman <kodiackerman@msn.com>
2019-08-22 15:03:02 -0700

    chore: updated tests to use "node-tdd"

--- test/logic/util/cfn-response-wrapper.spec.js
+++ test/logic/util/cfn-response-wrapper.spec.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
-const path = require('path');
 const expect = require('chai').expect;
-const nockBack = require('nock').back;
+const { describe } = require('node-tdd');
 const response = require('../../../src/logic/util/cfn-response-wrapper');
 
 const sampleEvent = {
@@ -20,33 +19,27 @@ const sampleEvent = {
 
 const logs = [];
 const consoleLogOriginal = console.log;
-nockBack.setMode('record');
 
-const doTest = (errObject, respObject, status, done) => {
-  nockBack(`cfb-response-wrapper-callback-${status.toLowerCase()}.json_recording.json`, {}, (nockDone) => {
-    response.wrap((event, context, callback, rb) => {
-      expect(rb).to.equal('rb');
-      callback(errObject, respObject);
-    })(sampleEvent, {}, (err, resp) => {
-      expect(err).to.equal(errObject);
-      expect(resp).to.equal(respObject);
-      expect(logs).to.deep.equal([
-        'Response body:\n',
-        `{"Status":"${status.toUpperCase()}","Reason":"See the details in CloudWatch Log Stream: undefined",`
-        + '"StackId":"arn:aws:cloudformation:eu-west-1:...","RequestId":"afd8d7c5-9376-4013-8b3b-307517b8719e",'
-        + '"LogicalResourceId":"Route53","Data":{}}',
-        'Status code: 200',
-        'Status message: null'
-      ]);
-      nockDone();
-      done();
-    }, 'rb');
-  });
+const doTest = (errObject, respObject, status) => {
+  response.wrap((event, context, callback, rb) => {
+    expect(rb).to.equal('rb');
+    callback(errObject, respObject);
+  })(sampleEvent, {}, (err, resp) => {
+    expect(err).to.equal(errObject);
+    expect(resp).to.equal(respObject);
+    expect(logs).to.deep.equal([
+      'Response body:\n',
+      `{"Status":"${status.toUpperCase()}","Reason":"See the details in CloudWatch Log Stream: undefined",`
+      + '"StackId":"arn:aws:cloudformation:eu-west-1:...","RequestId":"afd8d7c5-9376-4013-8b3b-307517b8719e",'
+      + '"LogicalResourceId":"Route53","Data":{}}',
+      'Status code: 200',
+      'Status message: null'
+    ]);
+  }, 'rb');
 };
 
-describe('Testing cfn-response-wrapper', () => {
+describe('Testing cfn-response-wrapper', { useNock: true }, () => {
   before(() => {
-    nockBack.fixtures = path.join(__dirname, '__cassette');
     console.log = (...args) => {
       logs.push(...args);
     };
@@ -59,11 +52,11 @@ describe('Testing cfn-response-wrapper', () => {
     logs.length = 0;
   });
 
-  it('Testing Callback Execution Success', (done) => {
-    doTest(null, 'response', 'SUCCESS', done);
+  it('Testing Callback Execution Success', () => {
+    doTest(null, 'response', 'SUCCESS');
   });
 
-  it('Testing Callback Execution Failure', (done) => {
-    doTest('err', undefined, 'FAILED', done);
+  it('Testing Callback Execution Failure', () => {
+    doTest('err', undefined, 'FAILED');
   });
 });
--- test/logic/util/lambda.spec.js
+++ test/logic/util/lambda.spec.js
@@ -1,89 +1,64 @@
-const path = require('path');
 const expect = require('chai').expect;
-const nockBack = require('nock').back;
-const lambdaInvalid = require('../../../src/logic/util/lambda')({
-  region: 'us-east-1',
-  accessKeyId: 'INVALID_ACCESS_KEY_ID',
-  secretAccessKey: 'INVALID_SECRET_ACCESS_KEY'
-});
-const lambda = require('../../../src/logic/util/lambda')({
-  region: 'us-east-1'
-});
+const { describe } = require('node-tdd');
+const LambdaInvalid = require('../../../src/logic/util/lambda');
+const Lambda = require('../../../src/logic/util/lambda');
 
 const func = { FunctionARN: 'FUNCTION_ARN', FunctionName: 'FUNCTION_NAME' };
 
-describe('Testing Lambda', () => {
+describe('Testing Lambda', { useNock: true }, () => {
+  let lambdaInvalid;
+  let lambda;
   before(() => {
-    nockBack.setMode('record');
-    nockBack.fixtures = path.join(__dirname, '__cassette');
-    process.env.AWS_ACCESS_KEY_ID = 'DUMMY';
-    process.env.AWS_SECRET_ACCESS_KEY = 'DUMMY';
-  });
-
-  after(() => {
-    delete process.env.AWS_ACCESS_KEY_ID;
-    delete process.env.AWS_SECRET_ACCESS_KEY;
-  });
-
-  it('Testing getAllFunctions Error', (done) => {
-    nockBack('lambda-get-all-functions-error.json_recording.json', {}, (nockDone) => {
-      lambdaInvalid.getAllFunctions().catch((e) => {
-        expect(e.message).to.equal('The security token included in the request is invalid.');
-        nockDone();
-        done();
-      });
+    lambdaInvalid = LambdaInvalid({
+      region: process.env.AWS_REGION,
+      accessKeyId: process.env.INTER_SERVICE_ACCESS_KEY_ID,
+      secretAccessKey: process.env.INTER_SERVICE_SECRET_ACCESS_KEY
     });
+    lambda = Lambda({ region: process.env.AWS_REGION });
   });
 
-  it('Testing setCloudWatchRetention Error', (done) => {
-    nockBack('lambda-get-set-cloud-watch-retention-error.json_recording.json', {}, (nockDone) => {
-      lambdaInvalid.setCloudWatchRetention(func, 30).catch((e) => {
-        expect(e.message).to.equal('The security token included in the request is invalid.');
-        nockDone();
-        done();
-      });
-    });
+  it('Testing getAllFunctions Error', async () => {
+    try {
+      await lambdaInvalid.getAllFunctions();
+    } catch (e) {
+      expect(e.message).to.equal('The security token included in the request is invalid.');
+    }
   });
 
-  it('Testing subscribeCloudWatchLogGroup Error', (done) => {
-    nockBack('lambda-subscribe-cloud-watch-log-group-error.json_recording.json', {}, (nockDone) => {
-      lambdaInvalid.subscribeCloudWatchLogGroup(func, func).catch((e) => {
-        expect(e.message).to.equal('The security token included in the request is invalid.');
-        nockDone();
-        done();
-      });
-    });
+  it('Testing setCloudWatchRetention Error', async () => {
+    try {
+      await lambdaInvalid.setCloudWatchRetention(func, 30);
+    } catch (e) {
+      expect(e.message).to.equal('The security token included in the request is invalid.');
+    }
   });
 
-  it('Testing appendLogRetentionInfo Error', (done) => {
-    nockBack('lambda-append-log-retention-info-error.json_recording.json', {}, (nockDone) => {
-      lambdaInvalid.appendLogRetentionInfo([func]).catch((e) => {
-        expect(e.message).to.equal('The security token included in the request is invalid.');
-        nockDone();
-        done();
-      });
-    });
+  it('Testing subscribeCloudWatchLogGroup Error', async () => {
+    try {
+      await lambdaInvalid.subscribeCloudWatchLogGroup(func, func);
+    } catch (e) {
+      expect(e.message).to.equal('The security token included in the request is invalid.');
+    }
   });
 
-  it('Testing appendLogSubscriptionInfo Error', (done) => {
-    nockBack('lambda-append-log-subscription-info-error.json_recording.json', {}, (nockDone) => {
-      lambdaInvalid.appendLogSubscriptionInfo([func]).catch((e) => {
-        expect(e.message).to.equal('The security token included in the request is invalid.');
-        nockDone();
-        done();
-      });
-    });
+  it('Testing appendLogRetentionInfo Error', async () => {
+    try {
+      await lambdaInvalid.appendLogRetentionInfo([func]);
+    } catch (e) {
+      expect(e.message).to.equal('The security token included in the request is invalid.');
+    }
   });
 
-  it('Testing getAllFunctions Batched', (done) => {
-    nockBack('lambda-get-all-functions-batched.json_recording.json', {}, (nockDone) => {
-      lambda.getAllFunctions({
-        ResourcesPerPage: 1
-      }).then((r) => {
-        expect(r.length).to.equal(2);
-        nockDone();
-        done();
-      }).catch(done.fail);
-    });
+  it('Testing appendLogSubscriptionInfo Error', async () => {
+    try {
+      await lambdaInvalid.appendLogSubscriptionInfo([func]);
+    } catch (e) {
+      expect(e.message).to.equal('The security token included in the request is invalid.');
+    }
+  });
+
+  it('Testing getAllFunctions Batched', async () => {
+    const result = await lambda.getAllFunctions({ ResourcesPerPage: 1 });
+    expect(result.length).to.equal(2);
   });
 });
