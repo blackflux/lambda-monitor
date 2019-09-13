aa26dab402869610201a12b46329295259a87f99
Jeff Soloshy <jeffsoloshy@gmail.com>
2019-09-13 15:40:52 -0700

    feat: now sending all non-excluded events to s3 bucket

--- src/logic/util/s3.js
+++ src/logic/util/s3.js
@@ -1,9 +1,18 @@
+const zlib = require('zlib');
 const aws = require('aws-sdk-wrap')();
 
 const s3 = aws.get('s3');
 
 const listObjectsV2 = (p) => s3.listObjectsV2(p).promise();
 const deleteObjects = (p) => s3.deleteObjects(p).promise();
+
+module.exports.putGzipObject = (bucket, key, data) => aws.call('s3:putObject', {
+  ContentType: 'application/json',
+  ContentEncoding: 'gzip',
+  Bucket: bucket,
+  Key: key,
+  Body: zlib.gzipSync(data, { level: 9 })
+});
 
 module.exports.emptyBucket = (objParams, logger) => {
   logger.info(`emptyBucket(): ${JSON.stringify(objParams)}`);
