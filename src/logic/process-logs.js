5f6fa714e7f90a10333a4699fa2a2eb12d916267
Jeff Soloshy <jeffsoloshy@gmail.com>
2019-09-13 10:59:27 -0700

    fix: now using aws-sdk-wrap, refactoring for extendability

--- src/logic/process-logs.js
+++ src/logic/process-logs.js
@@ -1,6 +1,6 @@
+const zlib = require('zlib');
 const get = require('lodash.get');
 const defaults = require('lodash.defaults');
-const zlibPromise = require('./util/zlib-promise');
 const timeoutPromise = require('./util/timeout-promise');
 const promiseComplete = require('./util/promise-complete');
 const logz = require('./services/logz');
@@ -34,54 +34,63 @@ const requestLogLevel = new RegExp([
   /^(?<logLevel>DEBUG|INFO|WARNING|ERROR|CRITICAL):/
 ].map((r) => r.source).join(''), '');
 
-module.exports = (event, context, callback, rb) => zlibPromise
-  .gunzip(Buffer.from(event.awslogs.data, 'base64'))
-  .then((r) => r.toString('ascii'))
-  .then(JSON.parse)
-  .then((resultParsed) => {
-    const toLog = [];
-    resultParsed.logEvents.forEach((logEvent) => {
-      const requestLog = requestLogRegex.exec(logEvent.message);
-      if (requestLog) {
-        const {
-          duration, billedDuration, maxMemory, requestId, memory, initDuration, traceId, segmentId, sampled
-        } = requestLog.groups;
-        toLog.push({
-          message: logEvent.message,
-          logGroupName: resultParsed.logGroup,
-          logStreamName: resultParsed.logStream,
-          owner: resultParsed.owner,
-          timestamp: new Date(logEvent.timestamp).toISOString(),
-          requestId,
-          duration: parseFloat(duration),
-          billedDuration: parseInt(billedDuration, 10),
-          maxMemory: parseInt(maxMemory, 10),
-          memory: parseInt(memory, 10),
-          initDuration: initDuration === undefined ? null : parseFloat(initDuration),
-          traceId,
-          segmentId,
-          sampled,
-          env: process.env.ENVIRONMENT
-        });
-      } else if (!logEvent.message.match(requestEndRegex) && !logEvent.message.match(requestStartRegex)) {
-        const processedLogEvent = defaults({ message: logEvent.message.replace(genericPrefix, '') }, logEvent);
-        const logLevel = get(
-          requestLogLevel.exec(processedLogEvent.message),
-          'groups.logLevel',
-          'WARNING'
-        ).toLowerCase();
-        rb[logLevel](processedLogEvent, process.env.ENVIRONMENT);
-      }
-    });
-    return [resultParsed, toLog];
-  })
-  .then(([resultParsed, toLog]) => {
-    const timeout = Math.floor((context.getRemainingTimeInMillis() - 5000.0) / 1000.0) * 1000;
-    return promiseComplete([
-      timeoutPromise(logz.log(context, process.env.ENVIRONMENT, toLog), timeout, 'logz'),
-      timeoutPromise(loggly.log(context, process.env.ENVIRONMENT, toLog), timeout, 'loggly'),
-      timeoutPromise(datadog.log(context, process.env.ENVIRONMENT, toLog), timeout, 'datadog')
-    ]).then(() => resultParsed);
-  })
-  .then((resultParsed) => callback(null, resultParsed))
-  .catch(callback);
+const getToLog = (resultParsed, rb) => {
+  const result = [];
+  resultParsed.logEvents.forEach((logEvent) => {
+    const requestLog = requestLogRegex.exec(logEvent.message);
+    if (requestLog) {
+      const {
+        duration, billedDuration, maxMemory, requestId, memory, initDuration, traceId, segmentId, sampled
+      } = requestLog.groups;
+      result.push({
+        message: logEvent.message,
+        logGroupName: resultParsed.logGroup,
+        logStreamName: resultParsed.logStream,
+        owner: resultParsed.owner,
+        timestamp: new Date(logEvent.timestamp).toISOString(),
+        requestId,
+        duration: parseFloat(duration),
+        billedDuration: parseInt(billedDuration, 10),
+        maxMemory: parseInt(maxMemory, 10),
+        memory: parseInt(memory, 10),
+        initDuration: initDuration === undefined ? null : parseFloat(initDuration),
+        traceId,
+        segmentId,
+        sampled,
+        env: process.env.ENVIRONMENT
+      });
+    } else if (!logEvent.message.match(requestEndRegex) && !logEvent.message.match(requestStartRegex)) {
+      const processedLogEvent = defaults({ message: logEvent.message.replace(genericPrefix, '') }, logEvent);
+      const logLevel = get(
+        requestLogLevel.exec(processedLogEvent.message),
+        'groups.logLevel',
+        'WARNING'
+      ).toLowerCase();
+      rb[logLevel](processedLogEvent, process.env.ENVIRONMENT);
+    }
+  });
+  return result;
+};
+
+const processLogs = async (event, context, rb) => {
+  const resultParsed = JSON.parse(zlib
+    .gunzipSync(Buffer.from(event.awslogs.data, 'base64'))
+    .toString('ascii'));
+
+  const toLog = getToLog(resultParsed, rb);
+  const timeout = Math.floor((context.getRemainingTimeInMillis() - 5000.0) / 1000.0) * 1000;
+  await promiseComplete([
+    timeoutPromise(logz.log(context, process.env.ENVIRONMENT, toLog), timeout, 'logz'),
+    timeoutPromise(loggly.log(context, process.env.ENVIRONMENT, toLog), timeout, 'loggly'),
+    timeoutPromise(datadog.log(context, process.env.ENVIRONMENT, toLog), timeout, 'datadog')
+  ]);
+  return resultParsed;
+};
+
+module.exports = async (event, context, callback, rb) => {
+  try {
+    callback(null, await processLogs(event, context, rb));
+  } catch (err) {
+    callback(err);
+  }
+};
