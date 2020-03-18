120230204551b9a369615de93d4ca8e57bfe60b4
Lukas Siemon <github@blackflux.com>
2020-03-17 17:56:23 -0700

    amend: more refactor

--- src/logic/process-logs.js
+++ src/logic/process-logs.js
@@ -13,37 +13,35 @@ const processLogs = async (event, context) => {
     .filter((logEvent) => !parser.isRequestStartOrEnd(logEvent.message))
     .map((logEvent) => [logEvent, parser.extractRequestMeta(logEvent.message)]);
 
-  await Promise.all([
-    ...logEvents
-      .filter(([logEvent, requestMeta]) => requestMeta === null)
-      .map(([logEvent]) => {
-        const { target, logLevel, message } = parser.extractLogMessage(logEvent.message, data.logGroup);
-        if (target !== 'rollbar') {
-          messageLogger(target, message);
-          return Promise.resolve();
-        }
-        const processedLogEvent = { ...logEvent, message };
-        const [year, month, day] = new Date(processedLogEvent.timestamp).toISOString().split('T')[0].split('-');
-        messageLogger(target, {
-          logGroup: data.logGroup,
-          logStream: data.logStream,
-          level: logLevel,
-          message: processedLogEvent.message,
-          timestamp: Math.floor(processedLogEvent.timestamp / 1000)
-        });
-        messageLogger(
-          's3',
-          process.env.LOG_STREAM_BUCKET_NAME,
-          `${data.logGroup.slice(1)}/${year}/${month}/${day}/${logLevel}-${logEvent.id}.json.gz`,
-          JSON.stringify(processedLogEvent)
-        );
-        return Promise.resolve();
-      }),
-    Promise.all(logEvents
-      .filter(([logEvent, requestMeta]) => requestMeta !== null)
-      .map(([logEvent, requestMeta]) => parser.generateExecutionReport(data, logEvent, requestMeta)))
-      .then((toLog) => metricLogger(context, process.env.ENVIRONMENT, toLog))
-  ]);
+  const messageLogs = logEvents.filter(([logEvent, requestMeta]) => requestMeta === null);
+  messageLogs.forEach(([logEvent]) => {
+    const { target, logLevel, message } = parser.extractLogMessage(logEvent.message, data.logGroup);
+    if (target !== 'rollbar') {
+      messageLogger(target, message);
+      return;
+    }
+    const processedLogEvent = { ...logEvent, message };
+    const [year, month, day] = new Date(processedLogEvent.timestamp).toISOString().split('T')[0].split('-');
+    messageLogger(target, {
+      logGroup: data.logGroup,
+      logStream: data.logStream,
+      level: logLevel,
+      message: processedLogEvent.message,
+      timestamp: Math.floor(processedLogEvent.timestamp / 1000)
+    });
+    messageLogger(
+      's3',
+      process.env.LOG_STREAM_BUCKET_NAME,
+      `${data.logGroup.slice(1)}/${year}/${month}/${day}/${logLevel}-${logEvent.id}.json.gz`,
+      JSON.stringify(processedLogEvent)
+    );
+  });
+
+  const metricLogs = await Promise.all(logEvents
+    .filter(([logEvent, requestMeta]) => requestMeta !== null)
+    .map(([logEvent, requestMeta]) => parser.generateExecutionReport(data, logEvent, requestMeta)));
+  metricLogger(context, process.env.ENVIRONMENT, metricLogs);
+
   await singletonLogger.flushAll();
   return data;
 };
