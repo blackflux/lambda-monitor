1a1f5cf9c1d9f6a2285882df43608470de0eb6fd
Kodi Ackerman <kodiackerman@msn.com>
2020-05-05 16:27:47 -0700

    amend: prepending logGroup

--- src/logic/logger/message/s3.js
+++ src/logic/logger/message/s3.js
@@ -1,13 +1,13 @@
 const s3PutGzipObject = require('../singleton/s3-put-gzip-object');
 
-module.exports = ({ message }) => {
+module.exports = ({ logGroup, message }) => {
   let messageParsed;
   try {
     messageParsed = JSON.parse(message);
     const { key, data } = messageParsed;
     s3PutGzipObject.enqueue(
       process.env.LOG_BUCKET_NAME,
-      key,
+      `${logGroup.slice(1)}/${key}`,
       JSON.stringify(data)
     );
   } catch (e) {
