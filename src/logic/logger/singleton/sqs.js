e5a9cf6d5d36eec56e795258924d9045669ac8c1
Lukas Siemon <github@blackflux.com>
2021-09-27 14:51:20 -0700

    amend: minor refactor

--- src/logic/logger/singleton/sqs.js
+++ src/logic/logger/singleton/sqs.js
@@ -3,14 +3,17 @@ const Sqs = require('../../util/sqs');
 const sqs = Sqs();
 
 module.exports = (() => {
-  const queue = [];
+  const buffer = {};
   return {
-    enqueue: (msg) => {
-      queue.push(msg);
+    enqueue: (url, msg) => {
+      if (!(url in buffer)) {
+        buffer[url] = [];
+      }
+      buffer[url].push(msg);
     },
-    flush: () => sqs.sendBatch({
-      messages: queue.splice(0),
-      queueUrl: process.env.BATCHER_QUEUE_URL
-    })
+    flush: () => Promise.all(Object.entries(buffer).map(([queueUrl, msgs]) => sqs.sendBatch({
+      queueUrl,
+      messages: msgs.splice(0)
+    })))
   };
 })();
