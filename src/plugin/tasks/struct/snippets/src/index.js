985cc8cd54bc4e80b78c96e25fe092c718b04519
Lukas Siemon <lukas.siemon@blackflux.com>
2022-09-01 10:53:36 -0700

    fix: adjusted for esm

--- src/plugin/tasks/struct/snippets/src/index.js
+++ src/plugin/tasks/struct/snippets/src/index.js
@@ -1,7 +1,7 @@
-import * as lambdaMonitor from 'lambda-monitor';
+import lambdaMonitor from 'lambda-monitor';
 
-export const batcherHandler = lambdaMonitor.batcherHandler;
-export const bundlerHandler = lambdaMonitor.bundlerHandler;
-export const processLogs = lambdaMonitor.processLogs;
-export const subscribe = lambdaMonitor.subscribe;
-export const emptyBucket = lambdaMonitor.emptyBucket;
+export const batcherHandler = lambdaMonitor.exports.batcherHandler;
+export const bundlerHandler = lambdaMonitor.exports.bundlerHandler;
+export const processLogs = lambdaMonitor.exports.processLogs;
+export const subscribe = lambdaMonitor.exports.subscribe;
+export const emptyBucket = lambdaMonitor.exports.emptyBucket;
