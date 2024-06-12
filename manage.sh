7324366fde7a0e04618f7aaddd8421935a838dc3
Lukas Siemon <lukas.siemon@blackflux.com>
2024-06-12 09:30:44 -0700

    fix: dep update

--- manage.sh
+++ manage.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+command=bash
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -c|--command) command="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; shift ;;
+    esac
+    shift
+done
+
 docker build \
+  --build-arg COMMAND="$command" \
   -t lambda-environment-node \
   --network="host" \
   docker/. &&
@@ -11,3 +22,7 @@ docker run \
   -v ~/.aws:/user/.aws \
   -v ~/.npmrc:/user/.npmrc \
   -it lambda-environment-node
+
+status=$?
+
+return $status
